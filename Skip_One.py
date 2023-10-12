1b257ce05a22c73f223bb24c554165f432ca0d70
purpledojo <137669631+purpledojo@users.noreply.github.com>
2023-10-12 17:53:25 +1300

    Add files via upload

---
+++ Skip_One.py
@@ -0,0 +1,11 @@
+ans = []
+fin = []
+num = input("give num or enter to stop")
+while num != "":
+    num = input("give num or enter to stop")
+    ans.append(num)
+#for m in range(0, len(ans), 2):
+ #   print (ans[m])
+  #  fin.append(ans[m])
+
+print(fin)
